2bea57e989decbb0cb798261b9f9086e56b04ecc
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-12-01 03:21:45 -0800

    7 islower python function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-   for i in str:
-       if ord(i) in range(97, 123):
-           cap = ord(i)-32
-        print('{:s}.format(chr(cap))', end='')
+    for i in str:
+        if ord(i) in range(97, 123):
+            cap = ord(i)-32
+        print('{:s}'.format(chr(cap)), end='')
     print('')
-
