385e9a29909331b3a86d575f071f3cac9383530e
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2022-12-07 05:02:24 -0800

    add task files

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return sorted(a_dictionary.keys())[-1]
-    else:
-        return None
+        return (max(a_dictionary))
