3735ba91a0a782187914b76c4cf30f25d5f4ad37
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2023-01-08 13:43:51 -0800

    last digit printer

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_last_digit(number):
+def print_last_digit(number:int)->int:
     if number < 0:
         return (10 - (number % 10))
-    return number % 10
+    return (number % 10)
