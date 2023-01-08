ae666e809e391cc64f48eb339edfb92c49e3d2a7
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2023-01-08 14:58:52 -0800

    last digit printer

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@ for letter in range(122, 96, -1):
     tmp = 0
     if letter % 2:
         tmp = 32
-        order = chr(letter - tmp)
+    order = chr(letter - tmp)
     print('{:s}'.format(order), end='')
