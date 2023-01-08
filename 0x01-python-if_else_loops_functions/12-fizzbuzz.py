cf9c48efebe3eeda89af26a4248501009fd68dc3
Masturah Muhammad Ghali <ronkeghali@gmail.com>
2023-01-08 14:16:53 -0800

    last digit printer

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz() -> None:
     for num in range(1, 101):
-        if( num % 15 == 0):
-            print("FizzBuzz", end = " ")
+        if(num % 15 == 0):
+            print("FizzBuzz", end=" ")
         elif(num % 3 == 0):
-            print("Fizz", end = ' ')
+            print("Fizz", end=' ')
         elif(num % 5 == 0):
-            print("Buzz", end = ' ')
+            print("Buzz", end=' ')
         else:
-            print(f'{num}', end = ' ')
+            print(f'{num}', end=' ')
