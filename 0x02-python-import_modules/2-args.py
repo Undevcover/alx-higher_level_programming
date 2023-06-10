027a451ab01fd38dd1ea5c44d1ab7e332ca6733c
Masturah Ghali <ronkeghali@gmail.com>
2023-06-10 08:31:39 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,15 @@
 from sys import argv
 
 def main():
-    length = len(argv) - 1
+    length = len(argv)
     punc = ":"
-    if (length == 0):
-        punc = "."
     if (length == 1):
+        punc = "."
+    if (length == 2):
         word = "arguement"
     else:
         word = "arguments"
-    print("{} {}{}".format(length, word, punc))
+    print("{} {}{}".format(length - 1, word, punc))
     for arg in argv:
         if (argv.index(arg) != 0):
             print("{}: {}".format(argv.index(arg), arg))
