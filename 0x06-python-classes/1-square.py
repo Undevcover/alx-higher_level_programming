53977ff42281fb71fc388cf320dec543eb65cd59
Masturah Ghali <ronkeghali@gmail.com>
2023-07-03 02:28:01 -0700

    0-square.py, 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Define a square with a private instance attribute """
 
-class Square:
-    def __init___(self, size):
-        size.__size = size
 
+class Square:
+    def __init__(self, size):
+        size.__size = size
